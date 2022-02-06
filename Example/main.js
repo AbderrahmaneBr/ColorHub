4cbb611f095cd74f3116054a19d41d07e77062c0
AbderrahmaneBr <96263038+AbderrahmaneBr@users.noreply.github.com>
2022-02-06 21:07:18 +0100

    Update main.js

--- Example/main.js
+++ Example/main.js
@@ -1,5 +1,5 @@
 import { Color } from '../ColorHub.js'
-
+ 
 var palette = Color().GenerateMatchingColorPalette("Monochromatic");
 for (let index = 0; index < palette.length; index++) {
     const element = palette[index];
@@ -13,3 +13,14 @@ for (let index = 0; index < rand_palette.length; index++) {
     document.getElementById("rand_"+index).style.backgroundColor = element;
     
 }
+
+var hex = document.getElementById("hex");
+hex.innerHTML = palette[0];
+var r = document.getElementById("r");
+var g = document.getElementById("g");
+var b = document.getElementById("b");
+
+var rgb = Color().toRGB(hex.innerHTML);
+r.innerHTML = rgb[0];
+g.innerHTML = rgb[1];
+b.innerHTML = rgb[2];
